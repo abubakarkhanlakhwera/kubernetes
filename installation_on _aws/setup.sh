48fc92fa25fffdac25d14e6d3067b475c88dcce5
Abubakar khan Lakhwera <109948432+abubakarkhanlakhwera@users.noreply.github.com>
2024-10-01 19:07:15 +0500

    Update setup.sh

--- installation_on _aws/setup.sh
+++ installation_on _aws/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status
-set -o pipefail  # Return the exit status of the last command in the pipe that failed
+
 
 # Update the system
 echo "Updating package lists..."
